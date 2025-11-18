5f0b509c56cfca51ad9327709f269317833ff9b0
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2025-11-18 20:14:49 +0100

    Checkstyle

--- src/test/java/com/hazelcast/spring/session/TestUtils.java
+++ src/test/java/com/hazelcast/spring/session/TestUtils.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hazelcast.spring.session;
 
 import com.hazelcast.internal.serialization.SerializationService;
